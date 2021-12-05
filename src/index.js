24e0b88997e20e0bf8ddc3b99545858e05cda2f4
Angie <amaticorena103@hotmail.com>
2021-12-04 19:58:05 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,27 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function renderOneToy(toys) {
+  toys.forEach(toy => {
+  let card = document.createElement('div')
+  card.className = 'card'
+  card.innerHTML =`
+    <h4>${toy.name}</h4>
+    <img src="${toy.image}" class='toy-avatar'>
+    <p>${toy.likes}</p>
+    <button class='like-button' id=${toy.id}>
+`
+  console.log(toy)
+  //console.log(card.innerHTML)
+  //console.log(toys.id)
+  })
+}
+
+document.addEventListener("DOMContentLoaded", fetchingToys)
+
+function fetchingToys() {
+  fetch("http://localhost:3000/toys")
+  .then(res =>res.json())
+  .then(toys => {renderOneToy(toys)});
+}
